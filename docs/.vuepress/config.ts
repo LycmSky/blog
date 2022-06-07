faa51c41c02d5f7e5b5ef13c5ffa24984e09b53c
lycm <lycmsky@qq.com>
2022-06-07 15:05:14 +0800

    修改网站标签栏图像

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "Lycm",
   description: "Lycm的博客",
-  head: [['link', { rel: 'icon', href: '/Icon/logo.svg' }]],
+  head: [['link', { rel: 'icon', href: '/blog/Icon/logo.svg' }]],
   base: "/blog/",
 
   theme,
